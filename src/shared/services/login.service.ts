1f9f9f9b88d8bb49b1a38ac97332280255450430
Stivenzxy <jesuspereztorres420@gmail.com>
2024-07-31 20:43:13 -0500

    cookies test #1

--- src/shared/services/login.service.ts
+++ src/shared/services/login.service.ts
@@ -42,7 +42,7 @@ export class LoginService {
   saveUserData(data: UserData) {
     try {
       const userData = JSON.stringify(data);
-      const cookieOptions: CookieOptions = { secure: true, path: '/' };
+      const cookieOptions: CookieOptions = { secure: false, path: '/' };
 
       if (data.is_admin) {
         this.cookieService.set('admin', userData, cookieOptions);
@@ -108,10 +108,7 @@ export class LoginService {
   }
 
   private logOutWithoutReload(): void {
-    this.cookieService.delete('admin', '/');
-    this.cookieService.delete('user', '/');
-    this.cookieService.delete('access_token', '/');
-    this.cookieService.delete('refresh_token', '/');
+    this.cookieService.deleteAll();
     localStorage.removeItem('register');
     this.adminAuthStatus.next(false);
     this.userAuthStatus.next(false);
@@ -129,7 +126,7 @@ export class LoginService {
   }
 
   private saveTokens(accessToken: string, refreshToken: string) {
-    const cookieOptions: CookieOptions = { secure: true, path: '/' };
+    const cookieOptions: CookieOptions = { secure: false, path: '/' };
     this.cookieService.set('access_token', accessToken, cookieOptions);
     this.cookieService.set('refresh_token', refreshToken, cookieOptions);
     this.currentAccessTkn.next(accessToken);
